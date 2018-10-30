743a261f6911c69c61225bd5cf348423c30f12ba
hufeng <huf01@mingyuanyun.com>
2018-10-30 18:21:42 +0800

    06条件渲染

---
+++ VueStudyExercise/06条件渲染.js
@@ -0,0 +1,12 @@
+﻿var vmLogin = new Vue({
+    el: "#login",
+    data: {
+        loginType:"username"
+    },
+    methods: {
+        toggleLoginType: function() {
+            var me = this;
+            return me.loginType = me.loginType === 'username' ? 'email' : 'username';
+        }
+    }
+})
